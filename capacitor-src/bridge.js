51f199d828317858c0147a8d1966c7c95d1ece1c
tracker <moviestudy900@gmail.com>
2026-09-08 00:16:48 +0600

    Add files via upload

--- capacitor-src/bridge.js
+++ capacitor-src/bridge.js
@@ -3,6 +3,7 @@ import { LocalNotifications } from '@capacitor/local-notifications';
 import { NativeBiometric } from 'capacitor-native-biometric';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Browser } from '@capacitor/browser';
+import { Share } from '@capacitor/share';
 
-window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Browser };
+window.CapBridge = { Capacitor, LocalNotifications, NativeBiometric, Filesystem, Directory, Browser, Share };
 
